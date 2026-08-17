4fc2992fc16e04752e352c2e1492592c12101e6f
Dmitriy <dimaarion@gmail.com>
2026-08-17 23:08:06 +0300

    Update MahjongDomino.jsx

--- src/components/MahjongDomino.jsx
+++ src/components/MahjongDomino.jsx
@@ -424,8 +424,8 @@ if(!start){
                        <text x={getSize(size,1680,840)} y={getSize(size,50,50)} transform={`translate(${-currentLevel.toString().length * 32} 0)`} width={"auto"} height={"auto"} fontSize={getSize(size,50,50)} fill={"url(#gradient_title)"} >Уровень {currentLevel}</text>
                        <g transform={`translate(${getSize(size,860,840)} ${getSize(size,90,120)})`}>
                            <g fontSize={getSize(size,45,45)} fill={"url(#gradient_title)"} >
-                               <g transform={'translate(100 8)'} filter={"url(#filter_combo_1)"}>
-                                   <animated.ellipse  fill={"#E2ED11"}  cx={0} cy={0} ry={3} style={styleCombo}  />
+                               <g transform={'translate(100 8)'} >
+                                   <animated.ellipse filter={"url(#filter_combo_1)"} fill={"#E2ED11"}  cx={0} cy={0} ry={3} style={styleCombo}  />
                                </g>
                                <text> Комбо: x{combo}</text>
                            </g>
